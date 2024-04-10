7a022f61003b550ac3a4409c73ac6452cf147273
paturikaustubh <plutofeb115@gmail.com>
2024-04-10 22:35:47 +0530

    footer part 2

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,39 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./styles.css";
+import { useEffect } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export function Footer() {
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    tl.to(".connector-cover", {
+      scrollTrigger: {
+        trigger: ".connector",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: false,
+        toggleActions: "play none none none",
+      },
+      duration: 0.5,
+      width: "0%",
+    });
+  }, []);
+
   return (
     <footer className="bg-stone-950 lg:p-12 md:p-6 p-3 text-[#E2E0DF]">
-      <p className="text-5xl font-bold">Kaustubh Paturi</p>
+      <div className="flex items-center gap-12">
+        <p className="text-7xl font-bold">Let's connect!</p>
+        <div className="flex items-center grow">
+          <span className="me-circle" />
+          <div className="connector grow relative h-[2px] bg-white">
+            <span className="absolute connector-cover inset-0 size-full bg-black w-full" />
+          </div>
+          <span className="them-circle" />
+        </div>
+      </div>
     </footer>
   );
 }
