92f0c557062b9f1575548a1883a9a4a175c936ee
paturikaustubh <plutofeb115@gmail.com>
2024-05-12 00:43:19 +0530

    final test before sleep...

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
+console.log(
+  import.meta.env.VITE_API_KEY,
+  import.meta.env.VITE_AUTH_DOMAIN,
+  import.meta.env.VITE_PROJECT_ID,
+  import.meta.env.VITE_STORAGE_BUCKET,
+  import.meta.env.VITE_MESSAGING_SENDER_ID,
+  import.meta.env.VITE_APP_ID
+);
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
